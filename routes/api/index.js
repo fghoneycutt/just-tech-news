6fcb6322271c0a34545defb2c5c2c8c22f6e4059
fghoneycutt <fghoneycutt@gmail.com>
2022-08-28 17:55:08 -0400

    added upvote routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const Post = require('../../models/Post.js');
-const User = require('../../models/User.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);

10cbebe2e9165c96ae2e4c7dfb5e2c99423359aa
fghoneycutt <fghoneycutt@gmail.com>
2022-08-31 01:37:41 -0400

    created sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,7 +61,12 @@ router.post('/', (req, res) => {
     password: req.body.password
   })
     .then(dbUserData => {
-      res.json(dbUserData);
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json(dbUserData);
+      });
     })
     .catch(err => {
       console.log(err);
@@ -87,8 +92,12 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
 });
 
@@ -133,5 +142,13 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
